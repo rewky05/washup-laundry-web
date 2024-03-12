c87149b8febca557fc7baaec10d89f471cb93181
rewky05 <apur.rosales.swu@phinmaed.com>
2024-03-12 13:46:27 +0800

    fixed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="flex justify-center items-center text-red">
         <div>
           <img
-            src="/washup-logo.png"
+            src="public/logo/washup-logo.png"
             className="md:w-40 min-[280px]:w-[140px] w-[160px]"
           />
         </div>
